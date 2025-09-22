500d43c9c801efc0533852e8cc8e126df9f660ca
S3XMUSIC <155470343+S3XMUSIC@users.noreply.github.com>
2025-09-22 21:12:51 +0300

    Update data_loader.py
    Программа скачивает датасет из гугл диска, а затем выводит первые 10 строк ( все очень просто ). По возможности можно менять датасет, смотри внутри файла, как это сделать

--- data_loader.py
+++ data_loader.py
@@ -1,11 +1,14 @@
-import gdown
+
+
+import gdown # специально для Gdrive
 import pandas as pd
 
-FILE_ID = "1NPjKJoVKQWytdYYEIFn7WQGVL6Tljo_L"
+FILE_ID = "1NPjKJoVKQWytdYYEIFn7WQGVL6Tljo_L" # Тут вставляет ваш ID документа из гугл диска, если собственный дата сет
 URL = f"https://drive.google.com/uc?id={FILE_ID}"
 
 OUTPUT = "dataset.csv"  # Тут качаем файл
 gdown.download(URL, OUTPUT, quiet=False)
 
 raw_data = pd.read_csv(OUTPUT)
+
 print(raw_data.head(10)) # Тут выводим первые 10 значений из датасета
